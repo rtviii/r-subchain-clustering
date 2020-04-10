b6221cb71084badc501082ccf88071c95c56da74
tractatusviii <rtkushner@gmail.com>
2020-04-10 10:44:33 -0500

    refactored nbr tree. clustersets, graphs next

--- get_neighbor_tree.py
+++ get_neighbor_tree.py
@@ -4,29 +4,86 @@ import os
 import numpy as np
 import json
 
-# REFACTOR THIS UGLY PIECE OF SHIT
+
+def extract_nbrtree_from_tuples(nbrtuples=list):
+    nbrtree = {}
+    # Create keys
+    for seedpair in nbrtuples:
+        if not(seedpair[0] in nbrtree.keys()):
+            nbrtree[seedpair[0]] = []
+        if not(seedpair[1] in nbrtree.keys()):
+            nbrtree[seedpair[1]] = []
+
+    for pair in nbrtuples:
+        # if second chain of the two is not in the cluster of the first,
+        # add it to the cluster (they are neigbor, after all)
+        if pair[1] not in nbrtree[pair[0]]:
+            nbrtree[pair[0]].append(pair[1])
+        if pair[0] not in nbrtree[pair[1]]:
+            nbrtree[pair[1]].append(pair[0])
+    return nbrtree
 
 
+def verify_no_rna(nbrtree=dict, allchains=list, rnas=list):
+    for key in nbrtree.keys():
+        for polymer in rnas:
+            if polymer in nbrtree[key]:
+                print("RNA {} in protein-only neighbor tree!".format(polymer))
+                raise ValueError
+        if key not in allchains:
+            print("Key {} is not struct's chains: {}!".format(key, allchains))
+            raise ValueError
 
 
-def get_neighbor_tree(filename, rnas, radius):
-    filename = filename
-    filepath = os.path.realpath("./../.cif_models/{}.cif".format(filename))
+def get_metadata(nbrclusters=list, allchains=list, rnas=list):
+    # --------------------------------------------TALLYING-----------------------------------------------------
+    singularChains = []
+    mergedClusters = set([])
+    # Merge everything
+    for cluster in nbrclusters:
+        mergedClusters.update(cluster)
+    for chain in allchains:
+        if chain not in mergedClusters and chain not in rnas:
+            singularChains.append(chain)
 
-    requestedstruct = filename
+    def countClustersChains(clusters=list):
+        # returns the number of [chains, clusters]
+        chaincount = 0
+        clustercount = 0
+        for cluster in clusters:
+            clustercount += 1
+            for chain in cluster:
+                chaincount += 1
+        return [chaincount, clustercount]
+
+    totalcount = countClustersChains(nbrclusters)
+    return {
+        "n_clusters"        : totalcount[1],
+        "n_chains_clustered": totalcount[0],
+        "n_rnas"            : len(rnas),
+        "n_chains_total"    : len(allchains),
+        "n_chains_singular" : len(singularChains),
+        "singular_chains"   : singularChains,
+        "rnas"              : rnas,
+        "allchains "        : allchains
+    }
+
+
+def get_neighbor_tree(pdbid, rnas, radius):
+    filepath = os.path.realpath("./../.cif_models/{}.cif".format(pdbid))
+    requestedstruct = pdbid
     requestedradius = radius
     rnas_to_exclude = rnas
 
-    print("\n\n----------------------------------------------------------------------------------------")
-    print("# Attempting to identify clusters of structure [{}] with the radius of [{}] Å       ".format(
-        requestedstruct, radius))
+    print("Opening {}".format(filepath))
+    try:
+        with open(filepath) as f:
+            parser = PDB.FastMMCIFParser(QUIET=True)
+            structure = parser.get_structure(requestedstruct, f)
+    except:
+        print("Failed to open {}".format(filepath))
 
-    print("attempting to open {}".format(filepath))
-    with open(filepath) as f:
-        parser = PDB.FastMMCIFParser(QUIET=True)
-        structure = parser.get_structure(requestedstruct, f)
-        allchains = [chain.get_id() for chain in structure.get_chains()]
-    # unfold to atoms
+    allchains = [chain.get_id() for chain in structure.get_chains()]
     atomwise_struct = PDB.Selection.unfold_entities(structure, "A")
     structwide_ns = PDB.NeighborSearch(atomwise_struct, bucket_size=5)
     all_nbr_pairs = structwide_ns.search_all(radius, "C")
@@ -39,83 +96,28 @@ def get_neighbor_tree(filename, rnas, radius):
             pass
         else:
             i += 1
-
+    nbridtuples = [(nbrtuple[0].get_id(), nbrtuple[1].get_id())
+                   for nbrtuple in all_nbr_pairs]
     # so, the neighbor tree is needed to provide a basis for clusters
-    # only the chains, who have neighbors in common belong to the same cluster
-    # Neverfcking mind.  T Hamelryck says this is fast enough.
-    # So either rewrite in WAS or shut up and code on. Huh. Maybe i will.
-    nbrtree = {}
-    # if a key doesnt already exists in the tree, create it
-    for nbrtuple in all_nbr_pairs:
-        if not(nbrtuple[0].get_id() in nbrtree.keys()):
-            nbrtree[nbrtuple[0].get_id()] = []
-        if not(nbrtuple[1].get_id() in nbrtree.keys()):
-            nbrtree[nbrtuple[1].get_id()] = []
+    nbrtree = extract_nbrtree_from_tuples(nbridtuples)
+    verify_no_rna(nbrtree, allchains, rnas_to_exclude)
 
-    for nbrtuple in all_nbr_pairs:
-        if nbrtuple[1].get_id() not in nbrtree[nbrtuple[0].get_id()]:
-            nbrtree[nbrtuple[0].get_id()].append(nbrtuple[1].get_id())
-        if nbrtuple[0].get_id() not in nbrtree[nbrtuple[1].get_id()]:
-            nbrtree[nbrtuple[1].get_id()].append(nbrtuple[0].get_id())
+    # Extract clusters from the neighbortree
+    nbrclusters = [list(c)for c in clustersets(nbrtree, rnas_to_exclude)]
 
-    for key in nbrtree.keys():
-        for polymer in rnas_to_exclude:
-            if polymer in nbrtree[key]:
-                print("RNA {} in protein-only neighbor tree!".format(polymer))
-                raise ValueError
-        if key not in allchains:
-            print("Key {} is not struct's chains: {}!".format(key, allchains))
-            raise ValueError
+    metadata = get_metadata(nbrclusters, allchains, rnas_to_exclude)
 
-    # Set is not JSON Serializable. Parse to arrays for now, use pickle or yml later.
-    nbr_clusters = [list(c)
-                    for c in clustersets(nbrtree, rnas_to_exclude)]
-
-    # --------------------------------------------TALLYING-----------------------------------------------------
-    singularChains = []
-    mergedClusters = set([])
-    for cluster in nbr_clusters:
-        mergedClusters.update(cluster)
-    for chain in allchains:
-        if chain not in mergedClusters and chain not in rnas_to_exclude:
-            singularChains.append(chain)
-
-    def totalchains(clusters=list):
-        # returns the number of [chains, clusters]
-        c = 0
-        n = 0
-        for cluster in clusters:
-            n += 1
-            for chain in cluster:
-                c += 1
-        return [c, n]
-    total = totalchains(nbr_clusters)
-    print("{} RNA polymers(excluded from clustering): {}".format(
-        len(rnas_to_exclude), rnas_to_exclude))
-    print("{} proteins in {} clusters: {}".format(
-        total[0], total[1], nbr_clusters))
-    print("{} singular proteins: {}".format(
-        len(singularChains), singularChains))
-    print("Nominal # of chains in the molecule: {}".format(len(allchains)))
-
-    # --------------------------------------------------------------------------------------------------------
-
-    # print('TOTAL 1 IS ', total[])
-    return [ total[0],   len(singularChains), total[1], {"clusters": nbr_clusters, "singular": singularChains}]
-
-
-# x = get_neighbor_tree('3j9m', ['A', 'B', 'u', 'AA'], 2.5)
-# print("X 3  IS :                    >>>>> ", x[3])
-
+    return {
+        "metadata": metadata,
+        "clusters": nbrclusters
+    }
 
 # >>>>>Past tries, emailed Thomas Hamelryck, the author of NeighborSearch among other things.
 # >>>> Still not sure if chain neighbors given a CHAIN could be gotten.
 # >>>> center in search has to be np.shape = (3,)
 # atomwise_K =        PDB.Selection.unfold_entities(subchain_dict['K'], 'A')
-
 # np_atomwise_K =     np.array([atom.get_coord() for atom in list(atomwise_K)])
 # struct_ns =         PDB.NeighborSearch(atomwise_struct, 10)
 # print(np.shape(np_atomwise_K))                                   # (1451,3)
-
 # chain_nbrs_single = struct_ns.search(np_atomwise_K[43], 10, "C") # works well, yields neighbors in 10 of 43'rd atom
 # chain_nbrs = struct_ns.search(np_atomwise_K, 10, "C")            # Exepected a 3-dimensional Numpy array.
