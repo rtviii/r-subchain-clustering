33c0d334ca2e6fefc3429b26a2361c8ed687080a
tractatusviii <rtkushner@gmail.com>
2020-04-20 00:50:15 -0500

    stuck with cuthill-mckee. Covariance matrices and presentation tomorrow

--- graph_algorithms/fiddling.py
+++ graph_algorithms/fiddling.py
@@ -9,25 +9,28 @@ from namespaces.total import total
 import functools
 import seaborn as sns
 import scipy.sparse as ssp
-from casadi import * 
+from casadi import *
 
 matplotlib.use("TkAgg")
 namespace = total
 
 # TARGET GROUPS:
 
-bacteria = ['5NJT',"5AFI", "5JVG", "5MYJ","5O60","5V7Q","5NRG","4Y4P","3J7Z", "5VP2"]
-eukarya  = ["6EK0","5T2A", "3J79","5GAK","4V7E","5T5H","5XXB", "5XY3", "4UGO"]
+bacteria = ['5NJT', "5AFI", "5JVG", "5MYJ",
+            "5O60", "5V7Q", "5NRG", "4Y4P", "3J7Z", "5VP2"]
+eukarya = ["6EK0", "5T2A", "3J79", "5GAK",
+           "4V7E", "5T5H", "5XXB", "5XY3", "4UGO"]
 
-#tight: 6eko, 5t5h
+# tight: 6eko, 5t5h
 
-#large: 4v7e(e), 
+# large: 4v7e(e),
+
 
 def construct_adjacency_matrix(clusters=dict, nomenclature_namespace=dict, verbose=False):
 
-    nbrpairs  = tree2tuplearr(clusters['nbrtree'])
-    dim       = len(nomenclature_namespace.items())
-    keys      = list(nomenclature_namespace.keys())
+    nbrpairs = tree2tuplearr(clusters['nbrtree'])
+    dim = len(nomenclature_namespace.items())
+    keys = list(nomenclature_namespace.keys())
     substrate = np.zeros((dim, dim))
 
     for pair in nbrpairs:
@@ -79,17 +82,16 @@ def test():
     # print(G.edges())
     # print(G['uS13']['bL31'])
     er = nx.erdos_renyi_graph(100, 0.15)
-    ws = nx.watts_strogatz_graph(30, 3, 0.1) 
+    ws = nx.watts_strogatz_graph(30, 3, 0.1)
     K_5 = nx.complete_graph(5)
-    pete =nx.petersen_graph()
+    pete = nx.petersen_graph()
     # nx.draw(G, with_labels=True,)
 
     A = nx.to_numpy_matrix(G)
     ScipA = csr_matrix(A)
-    print( ScipA )
+    print(ScipA)
     perm = ssp.csgraph.reverse_cuthill_mckee(ScipA)
 
-    
     arr_perm = A[perm, perm]
     # plt.spy(arr_perm )
     print(arr_perm.shape)
@@ -99,38 +101,47 @@ def test():
     plt.show()
 
 
-
 def get_laplacian_ndarray(nparray):
     csr = csr_matrix(nparray)
-    L   = ssp.csgraph.laplacian(csr)
+    L = ssp.csgraph.laplacian(csr)
     return L.toarray()
-
 
 
 def get_cuthill_mckee_ndarray(nparray):
 
     csr = csr_matrix(nparray)
-    cuthillmckee   = ssp.csgraph.reverse_cuthill_mckee(csr)
-    return cuthillmckee.toarray()
+    perm = ssp.csgraph.reverse_cuthill_mckee(csr)
+    bfs = nparray[perm[:, None], perm]
+    return bfs
 
 
 def get_simplemean(targetgroup=str):
     targetspath = './../clusterdata/targetgroups/{}/'.format(targetgroup)
-    batch       = os.listdir(targetspath)
-    print("BATCH >>>>", batch)
-    adjmats     = [construct_adjacency_matrix(openjson(targetspath+x), namespace, False) for x in batch]
-    
-    # laplacians  = []
-    reduced = functools.reduce(lambda x, y: np.add(x,y), adjmats)
+    batch = os.listdir(targetspath)
+    # print("BATCH >>>>", batch)
+    adjmats = [construct_adjacency_matrix(
+        openjson(targetspath+x), namespace, False) for x in batch]
+    reduced = functools.reduce(lambda x, y: np.add(x, y), adjmats)
     reduced = np.divide(reduced, len(adjmats))
 
-    L = get_laplacian_ndarray(reduced)
-    print(L)
+    # ----------------
 
-    sns.clustermap(L)
-    # plt.matshow(L)
+    L = get_laplacian_ndarray(reduced)
+    # print(L)
+    # perm, preds = ssp.csgraph.breadth_first_order(L, 0, directed=False)
+    # print(perm)
+    # print(preds)
+    # bfs = reduced[perm[:, None], perm]
+    bfs = get_cuthill_mckee_ndarray(reduced)
+    # print(bfs)
+
+    plt.matshow(bfs)
+    # sns.clustermap(bfs)
     plt.show()
     return
 
 
-get_simplemean('eukarya')
+
+#TODO: Cov matrix
+#TODO: Styling, names on the heatmap
+get_simplemean('melange')
