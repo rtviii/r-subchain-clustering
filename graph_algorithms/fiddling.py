766f2fb2307dd4f8c7c8134c0d9e5b9fcf4aa973
tractatusviii <rtkushner@gmail.com>
2020-04-21 00:24:15 -0500

    will need separate covariance matrices for eukaryotes, bacteria, small, large subunits

--- graph_algorithms/fiddling.py
+++ graph_algorithms/fiddling.py
@@ -1,20 +1,24 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 import matplotlib
 import json
 import os
 from scipy.sparse import csr_matrix
-from namespaces.total import total
+from namespaces.total import total, totalArr
 import functools
 import seaborn as sns
 import scipy.sparse as ssp
-from casadi import *
 
 matplotlib.use("TkAgg")
+
+
 namespace = total
+namespaceArr = totalArr
 
 # TARGET GROUPS:
+
 
 bacteria = ['5NJT', "5AFI", "5JVG", "5MYJ",
             "5O60", "5V7Q", "5NRG", "4Y4P", "3J7Z", "5VP2"]
@@ -107,15 +111,20 @@ def get_laplacian_ndarray(nparray):
     return L.toarray()
 
 
-def get_cuthill_mckee_ndarray(nparray):
+def get_cuthill_mckee_ndarray(nparray, names):
 
+    print('BEFORE PETM', names)
     csr = csr_matrix(nparray)
     perm = ssp.csgraph.reverse_cuthill_mckee(csr)
     bfs = nparray[perm[:, None], perm]
-    return bfs
+
+    permutednames = [names[i] for i in perm]
+
+    print('after perm', permutednames)
+    return bfs, permutednames
 
 
-def get_simplemean(targetgroup=str):
+def get_simplemean(targetgroup=str, cmkee=bool):
     targetspath = './../clusterdata/targetgroups/{}/'.format(targetgroup)
     batch = os.listdir(targetspath)
     # print("BATCH >>>>", batch)
@@ -125,23 +134,58 @@ def get_simplemean(targetgroup=str):
     reduced = np.divide(reduced, len(adjmats))
 
     # ----------------
-
     L = get_laplacian_ndarray(reduced)
-    # print(L)
-    # perm, preds = ssp.csgraph.breadth_first_order(L, 0, directed=False)
-    # print(perm)
-    # print(preds)
-    # bfs = reduced[perm[:, None], perm]
-    bfs = get_cuthill_mckee_ndarray(reduced)
-    # print(bfs)
 
-    plt.matshow(bfs)
-    # sns.clustermap(bfs)
+    fig, ax = plt.subplots()
+    if cmkee == True:
+        bfs, permnames = get_cuthill_mckee_ndarray(reduced, namespaceArr)
+
+        im = ax.imshow(bfs)
+        ax.set_xticks(np.arange(len(permnames)))
+        ax.set_yticks(np.arange(len(permnames)))
+        ax.set_yticklabels(permnames)
+        ax.set_xticklabels(permnames)
+    else:
+        im = ax.imshow(reduced)
+        ax.set_xticks(np.arange(len(namespaceArr)))
+        ax.set_yticks(np.arange(len(namespaceArr)))
+        ax.set_yticklabels(namespaceArr)
+        ax.set_xticklabels(namespaceArr)
+
+    plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
+             rotation_mode="anchor", fontsize=4)
+
+    plt.setp(ax.get_yticklabels(),  ha="right",
+             rotation_mode="anchor", fontsize=4)
+    ax.set_title("Protein correlations")
+    fig.tight_layout()
+    # figure(figsize=(6,8))
+
     plt.show()
     return
 
+# TODO: Cov matrix
+# TODO: Styling, names on the heatmap
 
+def covmat(targetgroup):
+    targetspath = './../clusterdata/targetgroups/{}/'.format(targetgroup)
+    batch = os.listdir(targetspath)
+    # print("BATCH >>>>", batch)
+    adjmats = [construct_adjacency_matrix(
+        openjson(targetspath+x), namespace, False) for x in batch]
+    reduced = functools.reduce(lambda x, y: np.add(x, y), adjmats)
+    reduced = np.divide(reduced, len(adjmats))
+    covmat = np.cov(reduced)
 
-#TODO: Cov matrix
-#TODO: Styling, names on the heatmap
-get_simplemean('melange')
+    fig, ax = plt.subplots()
+    im = ax.imshow(covmat)
+    ax.set_xticks(np.arange(len(namespaceArr)))
+    ax.set_yticks(np.arange(len(namespaceArr)))
+    ax.set_yticklabels(namespaceArr)
+    ax.set_xticklabels(namespaceArr)
+
+    plt.show()
+    return
+
+# get_simplemean('melange', False)
+covmat('melange')
